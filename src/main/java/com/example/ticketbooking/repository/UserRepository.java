9fa5c3407ef7534eafa22e776ba5ca8723183afd
anshul <anshultyagi55562@gmail.com>
2025-08-06 23:26:11 +0530

    used Optional

--- src/main/java/com/example/ticketbooking/repository/UserRepository.java
+++ src/main/java/com/example/ticketbooking/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.example.ticketbooking.repository;
 
 import com.example.ticketbooking.Entities.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // You can add custom query methods here, e.g.:
-    // Optional<User> findByName(String name);
+    Optional<User> findByUserName(String userName);
 }
