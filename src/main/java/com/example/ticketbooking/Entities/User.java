46fc9248d51e025b67803f457840be6b691bfc02
anshul <anshultyagi55562@gmail.com>
2025-08-06 23:25:03 +0530

    implemented UserDetails in User class for spring security,

--- src/main/java/com/example/ticketbooking/Entities/User.java
+++ src/main/java/com/example/ticketbooking/Entities/User.java
@@ -1,6 +1,7 @@
 package com.example.ticketbooking.Entities;
 
 import java.util.List;
+import java.util.Collection;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,20 +9,53 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Getter;
 import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "users") // "user" is often a reserved keyword in SQL
-public class User {
+@Table(name = "users")
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
+    private String userName;
     private String password;
     @OneToMany(mappedBy = "user")
     private List<Ticket> ticketsBooked;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // For now, we're not using roles. Return an empty list.
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
